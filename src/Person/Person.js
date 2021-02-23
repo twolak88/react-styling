b1bc72d7dc7202d67da76e8a3781bf9b56aa8cf9
twolak88 <twolak88@gmail.com>
2021-02-23 23:51:15 +0100

    CSS Modules & Media Queries in Person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,28 +1,14 @@
-import styled from 'styled-components';
-
-//return react component
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
   return (
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         He is {props.name} and he is {props.age}!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
