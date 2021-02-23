fa8f6b35be7228ab44944433c74214b92f1094ec
twolak88 <twolak88@gmail.com>
2021-02-23 20:37:30 +0100

    initial project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'id1', name: 'Max', age: 28 },
+      { id: 'id2', name: 'Mat', age: 23 },
+      { id: 'id3', name: 'Tom', age: 31 },
+    ],
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = {
+      ...this.state.persons[personIndex],
+      name: event.target.value
+    }
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons: persons
+    });
+  }
+
+  deletePersonHandler = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({
+      persons: persons
+    });
+  }
+
+  togglePersonsHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    });
+  }
+
+  render() {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+    
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, idx) => {
+            return <Person 
+              name={ person.name } 
+              age={ person.age }
+              click={ this.deletePersonHandler.bind(this, idx) }
+              key={ person.id } 
+              changed={ (event) => this.nameChangedHandler(event, person.id) }/>;
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className='App'>
+        <h1>Hi, this is my first react app!!</h1>
+        <h1>another heading can be add only insight App</h1>
+        <button style={buttonStyle} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        { persons }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,15 @@
+import './Person.css';
+
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p onClick={ props.click }>
+        He is { props.name } and he is { props.age }!
+      </p>
+      <p>{ props.children }</p>
+      <input type="text" onChange={ props.changed } value={ props.name } />
+    </div>
+  );
+};
+
+export default person;
